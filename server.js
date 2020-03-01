db6fdd569ef4a0ddd5b741a9264fb467798db64c
Tomasz Steć <tomeczekstecc@gmail.com>
2020-03-01 15:32:41 +0100

    df

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.use('/api/v1/transactions', transactions);
 
 if ((process.env.NODE_ENV = 'production')) {
   app.use(express.static('client/build'));
-  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build'))
+  );
 }
 
 // app.get('/', (req, res) => {
